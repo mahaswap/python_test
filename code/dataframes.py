d5eac0039290477cc53378ae2cabec9b33e06698
swapnil <swapnil@lji.org>
2018-12-25 14:24:12 -0800

    Added df subsets.

--- code/dataframes.py
+++ code/dataframes.py
@@ -14,10 +14,20 @@ df=pd.DataFrame(lst[1:], columns=lst[0])
 lst2=[['c','d','e'], [7,8,9], [10,11,12]]
 df2=pd.DataFrame(lst2[1:], columns=lst2[0])
 
+# Create df from dict
+dict1={'col1':['r1','r2','r3','r3'], 'col2':[5,10,15,20]}
+df3=pd.DataFrame.from_dict(dict1)
+
+# Subset of the df
+df3[df3.col2>=10]
+# parentheses are important when you have more than 2 conditions
+# AND condition
+df3[(df3.col2>=10) & (df3.col1=='r3')]
+# OR condition
+df3[(df3.col2>10) | (df3.col1=='r1')]
 
 # Appending to the dataframe
 df.append(df2)
-df.append(df2, ignore_index=True)
 df.append(df2, ignore_index=True)
 df_appeneded=df.append(df2, ignore_index=True)
 
