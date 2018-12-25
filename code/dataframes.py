2cb7d614417dded1448fca5d3d6fb3388f4936c3
swapnil <swapnil@lji.org>
2018-12-25 15:28:12 -0800

    Added stats and boxplot.

--- code/dataframes.py
+++ code/dataframes.py
@@ -6,17 +6,39 @@ Created on Fri Nov 16 17:26:33 2018
 """
 
 import pandas as pd
+import numpy as np
+
 # Create df from a list of lists
 lst=[['a','b','c'],[1,2,3],[4,5,6]]
 df=pd.DataFrame(lst)
 df=pd.DataFrame(lst[1:], columns=lst[0])
-
-lst2=[['c','d','e'], [7,8,9], [10,11,12]]
-df2=pd.DataFrame(lst2[1:], columns=lst2[0])
+# df dimensions
+print(df.shape)
+# df columns
+print(list(df.columns))
+# element
+print(df.loc[0,'a'])
+# column
+print(df['a'])
 
 # Create df from dict
 dict1={'col1':['r1','r2','r3','r3'], 'col2':[5,10,15,20]}
 df3=pd.DataFrame.from_dict(dict1)
+
+
+# Summary stats of all cols
+df.describe()
+# Quantiles
+# 90th percentile
+df.a.quantile(0.9)
+# apply
+df.apply(np.sqrt)
+# Aggregate
+df.aggregate(['min','mean','median','max'])
+#boxplot of all cols
+df.boxplot()
+# Correlation coefficients: all against all 
+
 
 # Subset of the df
 df3[df3.col2>=10]
@@ -25,6 +47,16 @@ df3[df3.col2>=10]
 df3[(df3.col2>=10) & (df3.col1=='r3')]
 # OR condition
 df3[(df3.col2>10) | (df3.col1=='r1')]
+
+
+# Appending and merging
+
+lst2=[['c','d','e'], [7,10,15], [14,20,30], [21,30,45]]
+df2=pd.DataFrame(lst2[1:], columns=lst2[0])
+df.corr()
+
+# Correlation of df with df2 : row-wise
+df2.corrwith(df, axis=0)
 
 # Appending to the dataframe
 df.append(df2)
